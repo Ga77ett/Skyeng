8cfebb8bc4cbdfdbc92032b8e188b4b549d0e61f
Kirill Shalankin <kirill@shalankin.me>
2020-09-15 11:41:45 +0300

    Initial Commit

---
+++ Skyeng/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Skyeng
+//
+//  Created by Kirill Shalankin on 15.09.2020.
+//  Copyright © 2020 Kirill Shalankin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
